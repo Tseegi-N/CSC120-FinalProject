7ec08298463e45e1d66b85aa8ce25f04fbaf3dbf
Tseegi_N <tnyamdorj@smith.edu>
2022-11-29 15:59:46 -0500

    Set-up of tiles

--- Actions.java
+++ Actions.java
@@ -10,6 +10,7 @@ class Game implements Actions{
     public String c;
     public String questions;*/
 
+    Location firstTile = new Location(1, "Do you brush your teeth in the morning?", 1, "yes", "no", "rinse with moutwash");
     /* Constructor */
     public Game(){
         System.out.println("Welcome to the Good Place: the game!");
@@ -17,9 +18,22 @@ class Game implements Actions{
         System.out.println("Your every action in this lifetime decides whether you go into \n the good place or the bad place. Play the dice, go to the tiles, and answer questions to gain point. \n There is a total of 40 tiles until you reach Judge's chamber.");
         System.out.println("Good forking luck!");
         System.out.println("");
+
+        boolean stillPlaying = true;
+    
+        // The do...while structure means we execute the body of the loop once before checking the stopping condition
+        do {
+            System.out.println("You are still playing. Follow the instructions if you want to win/lose...");
+            
+            if (0 == 0) {
+                stillPlaying = false;
+            }
+        } while (stillPlaying);
     }
 
-    public void buy(String questions, String a, String b, String c){
+    
+
+    /*public void buy(String questions, String a, String b, String c){
         System.out.println(questions);
         System.out.println("Choices are: " + a + b + c);
         Scanner myObj = new Scanner(System.in);  
@@ -33,6 +47,8 @@ class Game implements Actions{
         }
         myObj.close();
     }
+    */
+
 
 
     public static void main(String[] args) {
--- Location.java
+++ Location.java
@@ -1,13 +1,45 @@
+import java.util.ArrayList;
+
 public class Location {
     String question;
     String[] answers;
-    Integer CorrectAns;
+    Integer CorrectAnswer;
+    Integer TileNumber;
 
-    public Location(String quest, String[] questAns, Integer CorrectAns){
+    public Location(Integer TileNum, String quest, Integer CorrectAns, String... questAns){
         this.question = quest;
-        this.answers[] = questAns[];
-        this.CorrectAns = CorrectAns;
+        this.answers = questAns;
+        this.CorrectAnswer = CorrectAns;
+        this.TileNumber = TileNum;
     }
+
+    public String getQuest(Integer TileNum){
+        if(TileNum == TileNumber){
+            return question;
+        }
+        else{
+            return "wtf??";
+        }
+    }
+
+    public String[] getAns(Integer TileNum){
+        if(TileNum == TileNumber){
+            return answers;
+        }
+        else{
+            String[] example = {"m", "fe"};
+            return example;
+        }
+    }
+
+    public Integer getCorrectAns(Integer TileNum){
+        if(TileNum == TileNumber){
+            return CorrectAnswer;
+        }
+        else{
+            return -1;
+        }
+    }    
 
     public void askQuestions(){
         System.out.println(question);
