0ab6d901c04afd402c7e199c2146c12d9c539c37
Tseegi_N <tnyamdorj@smith.edu>
2022-12-02 08:46:50 -0500

    Correct tile

--- Actions.java
+++ Actions.java
@@ -16,47 +16,49 @@ class Game {
     static Integer DiceNumber;
     
 
-    Location firstTile = new Location(1, "Do you brush your teeth in the morning?", 0, "yes", "no", "rinse with moutwash");
-    Location secondTile = new Location(2, "How do you get to work?", 0, "bike", "bus", "drive");
-    Location thirdTile = new Location(3, "How do you interact with your coworkers?", 0, "Say hi", "ignore", "nod knowingly");
-    Location forthTile = new Location(4, "Buy food from grocery store. Which brand do you choose?", 0, "Organic Farmhouse", "Fresh Greens", "Happy Animals");
-    Location fifthTile = new Location(5, "Buy drink from grocery store. Which brand do you choose?", 2, "Bulb", "Flash", "Nurture");
-    Location sixthTile = new Location(6, "Buy snack from grocery store. Which brand do you choose?", 1, "Kringles", "Zheetos", "Jays");
-    Location seventhTile = new Location(7, "Buy appliances. Which brand do you choose?", 0, "Bouncy", "Cleanext", "Sweep");
-    Location eighthTile = new Location(8, "Buy a cookie from...", 0, "Local bakery", "Costco", "CVS");
-    Location ninthTile = new Location(9, "Buy a cookie. Which flavor do you choose?", 2, "Chocolate", "Almond", "Oatmeal");
-    Location tenthTile = new Location(10, "Buy a cup of coffee. What flavor do you choose?", 0, "Decaf", "Americano", "Latte");
-    Location eleventhTile = new Location(11, "Buy a cup of coffee. Where do you buy it from?", 0, "Local coffee shop", "Starbucks", "Dunkin");
-    Location twelfthTile = new Location(12, "Surprise your partner with flowers! Choose where you get your flowers from.", 0, "Handpick", "grocery store", "local flower shop");
-    Location thirteenthTile = new Location(13, "Check up on your introverted friend! What form of communication do you use?", 1, "Call", "Text", "Surprise visit!");
-    Location fourteenthTile = new Location(14, "Check up on your extroverted friend! What form of communication do you use?", 2, "Call", "Text", "Surprise visit!");
-    Location fifteenthTile = new Location(15, "You need to buy some toilet paper. Where do you want to buy it from?", 2, "Amazon", "Target", "Costco"); 
-    Location sixteenthTile = new Location(16, "Buy non dairy milk. Which type do you choose?", 2, "almond milk", "soy milk", "coconut milk");
-    Location seventeenthTile = new Location(17, "How do you ask someone out?", 1, "through phone", "surprise event", "not directly ask them out");
-    Location eighteenthTile = new Location(18, "If your friend ask you to be their bestmate/bridesmaid for their wedding: ", 1, "no", "yes", "says yes but regrets immediately afterwards");
-    Location ninteenthTile = new Location(19, "A volunteer handing out environmental awareness flyers", 0, "sign up to help", "ignore", "call them a \"whale hump\"");
-    Location twentiethTile = new Location(20, "Buy a drink!", 2, "Margarita", "Molotov cocktail", "wine");
-    Location twentyFirstTile = new Location(21, "How do you impress people?", 1, "Drop celebrity names", "tell jokes", "show them your moves");
-    Location twentySecondTile = new Location(22, "Which NFL team do you support?", 0, "New York Jets", "Miami Dolphins", "Jacksonville Jaguars");
-    Location twentyThirdTile = new Location(23, "How do you respond to 'I love you'?", 0, "YEET", "I consider you one of my favorite friends...", "who doesn't?!");
-    Location twentyFourthTile = new Location(24, "You want to do something. How do you proceed?", 2, "Immediately do it without thinking", "think it over for a year and give up", "ask your friend for help");
-    Location twentyFifthTile = new Location(25, "What is your favorite mythical animal?", 2, "Bearded dragon", "Penguin", "Unicorn");
-    Location twentySixthTile = new Location(26, "How do you get over a break-up?", 2, "Make a rebound guy(Derek)", "Pretend all is okay", "Drink margaritas");
-    Location twentySeventhTile = new Location(27, "How do you get out of the escape room?", 0, "Analyze all rules and play by the rule", "Get distracted by a butterfly and forget about the game", "Break enough stuff until they kick you out");
-    Location twentyEightTile = new Location(28, "What is one thing that brings you joy?", 0, "Wedding day", "People puking on roller coasters", "Vacation");
-    Location twentyNinthTile = new Location(29, "What is your favorite insult?", 2, "Shi(r)t for brains", "Ya basic", "You're a medium person");
-    Location thirtiethTile = new Location(30, "What do philosophers mean by 'knowing yourself'?", 1, "Masturbation", "Full understanding of who you are", "Inner peace");
-    Location thirtyFirstTile = new Location(31, "What do you do if your uber driver starts talking to you?", 0, "Talk with them", "Pay them to be quiet", "Argue that the ride should be free");
-    Location thirtySecondTile = new Location(32, "What do you do in your ethics class?", 1, "Sleep", "Take notes", "Question teacher on everything");
-    Location thirtyThirdTile = new Location(33, "What do you call your 'space'?", 1, "Bud-hole", "Buddy room", "Game room");
-    Location thirtyFourthTile = new Location(34, "What do you do when you have a problem?", 2, "Avoid at all cost", "Drink your problems away", "Approach gently");
-    Location thirtyFifthTile = new Location(35, "Would you feed the ducks?", 2, "No why would I??", "I could", "I can't attend work today because I have to do that");
-    Location thirtySixthTile = new Location(36, "Would you kick a baby?", 0, "No why would I??", "I could", "I can't attend work today because I have to do that");
-    Location thirtySeventhTile = new Location(37, "Would you buy 28 light bublbs from Home Depot?", 1, "No why would I??", "I could", "I can't attend work today because I have to do that");
-    Location thirtyEightTile = new Location(38, "What do you do if kids start yelling for McDonalds?", 0, "We have food at home", "Yell together", "Drives to McDonald drive away once your get a black coffee");
-    Location thirtyNinthTile = new Location(39, "What is the best human creation?", 0, "Froyo", "Chowder", "Garden gnomes");
-    Location fortiethTile = new Location(40, "What is the best tv show?", 1, "Game of Thrones", "The Good Place", "Friends");
-
+    Location[] map = new Location[41];{
+    map[1] = new Location("Do you brush your teeth in the morning?", 0, "yes", "no", "rinse with moutwash");
+    map[2] = new Location("How do you get to work?", 0, "bike", "bus", "drive");
+    map[3] = new Location("How do you interact with your coworkers?", 0, "Say hi", "ignore", "nod knowingly");
+    map[4] = new Location("Buy food from grocery store. Which brand do you choose?", 0, "Organic Farmhouse", "Fresh Greens", "Happy Animals");
+    map[5] = new Location("Buy drink from grocery store. Which brand do you choose?", 2, "Bulb", "Flash", "Nurture");
+    map[6] = new Location("Buy snack from grocery store. Which brand do you choose?", 1, "Kringles", "Zheetos", "Jays");
+    map[7] = new Location("Buy appliances. Which brand do you choose?", 0, "Bouncy", "Cleanext", "Sweep");
+    map[8] = new Location("Buy a cookie from...", 0, "Local bakery", "Costco", "CVS");
+    map[9] = new Location("Buy a cookie. Which flavor do you choose?", 2, "Chocolate", "Almond", "Oatmeal");
+    map[10] = new Location("Buy a cup of coffee. What flavor do you choose?", 0, "Decaf", "Americano", "Latte");
+    map[11] = new Location("Buy a cup of coffee. Where do you buy it from?", 0, "Local coffee shop", "Starbucks", "Dunkin");
+    map[12] = new Location("Surprise your partner with flowers! Choose where you get your flowers from.", 0, "Handpick", "grocery store", "local flower shop");
+    map[13] = new Location("Check up on your introverted friend! What form of communication do you use?", 1, "Call", "Text", "Surprise visit!");
+    map[14] = new Location("Check up on your extroverted friend! What form of communication do you use?", 2, "Call", "Text", "Surprise visit!");
+    map[15] = new Location("You need to buy some toilet paper. Where do you want to buy it from?", 2, "Amazon", "Target", "Costco"); 
+    map[16] = new Location("Buy non dairy milk. Which type do you choose?", 2, "almond milk", "soy milk", "coconut milk");
+    map[17] = new Location("How do you ask someone out?", 1, "through phone", "surprise event", "not directly ask them out");
+    map[18] = new Location("If your friend ask you to be their bestmate/bridesmaid for their wedding: ", 1, "no", "yes", "says yes but regrets immediately afterwards");
+    map[19] = new Location("A volunteer handing out environmental awareness flyers", 0, "sign up to help", "ignore", "call them a \"whale hump\"");
+    map[20] = new Location("Buy a drink!", 2, "Margarita", "Molotov cocktail", "wine");
+    map[21] = new Location("How do you impress people?", 1, "Drop celebrity names", "tell jokes", "show them your moves");
+    map[22] = new Location("Which NFL team do you support?", 0, "New York Jets", "Miami Dolphins", "Jacksonville Jaguars");
+    map[23] = new Location("How do you respond to 'I love you'?", 0, "YEET", "I consider you one of my favorite friends...", "who doesn't?!");
+    map[24]= new Location("You want to do something. How do you proceed?", 2, "Immediately do it without thinking", "think it over for a year and give up", "ask your friend for help");
+    map[25] = new Location("What is your favorite mythical animal?", 2, "Bearded dragon", "Penguin", "Unicorn");
+    map[26] = new Location("How do you get over a break-up?", 2, "Make a rebound guy(Derek)", "Pretend all is okay", "Drink margaritas");
+    map[27]  = new Location("How do you get out of the escape room?", 0, "Analyze all rules and play by the rule", "Get distracted by a butterfly and forget about the game", "Break enough stuff until they kick you out");
+    map[28] = new Location("What is one thing that brings you joy?", 0, "Wedding day", "People puking on roller coasters", "Vacation");
+    map[29] = new Location("What is your favorite insult?", 2, "Shi(r)t for brains", "Ya basic", "You're a medium person");
+    map[30]  = new Location("What do philosophers mean by 'knowing yourself'?", 1, "Masturbation", "Full understanding of who you are", "Inner peace");
+    map[31] = new Location("What do you do if your uber driver starts talking to you?", 0, "Talk with them", "Pay them to be quiet", "Argue that the ride should be free");
+    map[32] = new Location("What do you do in your ethics class?", 1, "Sleep", "Take notes", "Question teacher on everything");
+    map[33] = new Location("What do you call your 'space'?", 1, "Bud-hole", "Buddy room", "Game room");
+    map[34] = new Location("What do you do when you have a problem?", 2, "Avoid at all cost", "Drink your problems away", "Approach gently");
+    map[35] = new Location("Would you feed the ducks?", 2, "No why would I??", "I could", "I can't attend work today because I have to do that");
+    map[36] = new Location("Would you kick a baby?", 0, "No why would I??", "I could", "I can't attend work today because I have to do that");
+    map[37] = new Location("Would you buy 28 light bublbs from Home Depot?", 1, "No why would I??", "I could", "I can't attend work today because I have to do that");
+    map[38] = new Location("What do you do if kids start yelling for McDonalds?", 0, "We have food at home", "Yell together", "Drives to McDonald drive away once your get a black coffee");
+    map[39] = new Location("What is the best human creation?", 0, "Froyo", "Chowder", "Garden gnomes");
+    map[40] = new Location("What is the best tv show?", 1, "Game of Thrones", "The Good Place", "Friends");
+    }
+   
     /* Constructor */
     public Game(){
         System.out.println("Welcome to the Good Place: the game!");
@@ -66,9 +68,9 @@ class Game {
         System.out.println("");
     }
 
-    public void ask(){ 
-        System.out.println(firstTile.getQuest(1));
-        String[] answerOne = firstTile.getAns(1);
+    public void ask(Integer tile){ 
+        System.out.println(map[tile].getQuest());
+        String[] answerOne = map[tile].getAns();
         List <String> answerOneList = new ArrayList<String>();
         answerOneList = Arrays.asList(answerOne);
         String[] ansStr = answerOneList.toString().replace("[", "").replace("]", "").split(",");
@@ -90,14 +92,14 @@ class Game {
         }
     }
 
-    public void play(String reply){
-        String[] answerOne = firstTile.getAns(1);
+    public void play(String reply, Integer Tile){
+        String[] answerOne = map[Tile].getAns();
         System.out.println("Your answer: " + reply);
        if (reply.equalsIgnoreCase("a") || reply.equalsIgnoreCase("b") || reply.equalsIgnoreCase("c")){
-            if(firstTile.getCorrectAns(1) == multipleChoice(reply)){
+            if(map[Tile].getCorrectAns() == multipleChoice(reply)){
                 point += 6;
             }
-            if (firstTile.getCorrectAns(1) != multipleChoice(reply)){
+            if (map[Tile].getCorrectAns() != multipleChoice(reply)){
                 point -= 6;
             } 
         }else {
@@ -122,14 +124,12 @@ class Game {
             System.out.println("Roll the dice");
             Scanner myObj = new Scanner(System.in); 
             while(CurrentTile < 41){
-                
                 DiceNumber = dice();
                 CurrentTile += DiceNumber;
-                //Location(CurrentTile);
-                GoodPlace.ask(); 
+                GoodPlace.ask(CurrentTile); 
                 System.out.print("Please type your answer (a, b, c): ");
                 String reply = myObj.nextLine(); 
-                GoodPlace.play(reply); 
+                GoodPlace.play(reply, CurrentTile); 
                 System.out.println("Roll the dice again");
                 
             }
@@ -154,26 +154,4 @@ class Game {
         } while (stillPlaying);
 
     }
-
-    
-
-    /*public void buy(String questions, String a, String b, String c){
-        System.out.println(questions);
-        System.out.println("Choices are: " + a + b + c);
-        Scanner myObj = new Scanner(System.in);  
-        String reply = myObj.nextLine();  
-        System.out.println("You chose to buy " + reply);
-        if (reply.equalsIgnoreCase("a")){
-            point += 6;
-        }
-        if (reply.equalsIgnoreCase("b")){
-            point -= 6;
-        }
-        myObj.close();
-    }
-    */
-
-
-
-    
 }
--- Location.java
+++ Location.java
@@ -6,52 +6,21 @@ public class Location {
     Integer CorrectAnswer;
     Integer TileNumber;
 
-    public Location(Integer TileNum, String quest, Integer CorrectAns, String... questAns){
+    public Location(String quest, Integer CorrectAns, String... questAns){
         this.question = quest;
         this.answers = questAns;
         this.CorrectAnswer = CorrectAns;
-        this.TileNumber = TileNum;
     }
 
-    public Location(Integer TileNum){
-        this.TileNumber = TileNum;
+    public String getQuest(){
+        return question;
     }
 
-    public String getQuest(Integer TileNum){
-        if(TileNum == TileNumber){
-            return question;
-        }
-        else{
-            return "wtf??";
-        }
+    public String[] getAns(){
+        return answers;
     }
 
-    public String[] getAns(Integer TileNum){
-        if(TileNum == TileNumber){
-            return answers;
-        }
-        else{
-            String[] example = {"m", "fe"};
-            return example;
-        }
-    }
-
-    public Integer getCorrectAns(Integer TileNum){
-        if(TileNum == TileNumber){
-            return CorrectAnswer;
-        }
-        else{
-            return -1;
-        }
+    public Integer getCorrectAns(){
+        return CorrectAnswer;
     }    
-
-    public void askQuestions(Integer TileNum){
-        if(TileNum == TileNumber){
-            System.out.println(question);
-        }
-        else{
-            System.out.println("Invalid tile number");
-        }
-    }
-
 }
